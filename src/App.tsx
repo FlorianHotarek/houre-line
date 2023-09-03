9e726f19a6cbecbdebe16ffbcd08e0edde29a299
florian hotarek <flogax@gmail.com>
2023-09-03 13:53:29 +0200

    add some more libs and remove unused code add first header to test

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {AppBar, IconButton, Toolbar, Typography} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <AppBar position="static">
+            <Toolbar variant="dense">
+                <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" color="inherit" component="div">
+                    Photos
+                </Typography>
+            </Toolbar>
+        </AppBar>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createTheme } from '@mui/material/styles';
 import App from './App.tsx'
-import './index.css'
+import {CssBaseline, ThemeProvider} from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#1e88e5',
+        },
+        secondary: {
+            main: '#A000A6',
+        },
+    },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+      <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+      </ThemeProvider>
   </React.StrictMode>,
 )
